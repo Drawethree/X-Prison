aee3a5b1b0fc1ebcbdcec85acf62ada76351133a
Drawethree <janci.kluka@gmail.com>
2021-04-18 12:05:31 +0200

    1.3.18 -Scottay

---
+++ src/main/java/me/drawethree/ultraprisoncore/utils/MapUtil.java
@@ -0,0 +1,21 @@
+package me.drawethree.ultraprisoncore.utils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapUtil {
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue
+			(Map<K, V> map) {
+
+		return map.entrySet()
+				.stream()
+				.sorted(Map.Entry.<K, V>comparingByValue().reversed())
+				.collect(Collectors.toMap(
+						Map.Entry::getKey,
+						Map.Entry::getValue,
+						(e1, e2) -> e1,
+						LinkedHashMap::new
+				));
+	}
+}
