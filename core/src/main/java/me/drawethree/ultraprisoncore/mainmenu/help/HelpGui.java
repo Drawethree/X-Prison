169bd2114b85480585f01f6fcbfcdef33c815424
Drawethree <janci.kluka@gmail.com>
2021-11-21 03:20:24 +0100

    1.9.0-BETA Updated help GUI

--- core/src/main/java/me/drawethree/ultraprisoncore/mainmenu/help/HelpGui.java
+++ core/src/main/java/me/drawethree/ultraprisoncore/mainmenu/help/HelpGui.java
@@ -57,59 +57,64 @@ public class HelpGui extends Gui {
 			}
 
 			//AutoMiner
-			this.setItem(12, ItemStackBuilder.of(Material.DIAMOND_PICKAXE).name("&e&lAutoMiner").lore("&7Click to see detailed info", "&7about AutoMiner feature.").build(() -> {
+			this.setItem(11, ItemStackBuilder.of(Material.DIAMOND_PICKAXE).name("&e&lAutoMiner").lore("&7Click to see detailed info", "&7about AutoMiner feature.").build(() -> {
 				this.close();
 				new AutoMinerHelpGui(this.getPlayer()).open();
 			}));
 			//Autosell
-			this.setItem(13, ItemStackBuilder.of(SkullUtils.MONEY_SKULL.clone()).name("&e&lAutoSell").lore("&7Click to see detailed info", "&7about AutoSell feature.").build(() -> {
+			this.setItem(12, ItemStackBuilder.of(SkullUtils.MONEY_SKULL.clone()).name("&e&lAutoSell").lore("&7Click to see detailed info", "&7about AutoSell feature.").build(() -> {
 				this.close();
 				new AutoSellHelpGui(this.getPlayer()).open();
 			}));
 			//Enchants
-			this.setItem(14, ItemStackBuilder.of(CompMaterial.ENCHANTED_BOOK.toItem()).name("&e&lEnchants").lore("&7Click to see detailed info", "&7about Enchants feature.").build(() -> {
+			this.setItem(13, ItemStackBuilder.of(CompMaterial.ENCHANTED_BOOK.toItem()).name("&e&lEnchants").lore("&7Click to see detailed info", "&7about Enchants feature.").build(() -> {
 				this.close();
 				new EnchantsHelpGui(this.getPlayer()).open();
 			}));
 			//Gangs
-			this.setItem(20, ItemStackBuilder.of(SkullUtils.GANG_SKULL.clone()).name("&e&lGangs").lore("&7Click to see detailed info", "&7about Gangs feature.").build(() -> {
+			this.setItem(14, ItemStackBuilder.of(SkullUtils.GANG_SKULL.clone()).name("&e&lGangs").lore("&7Click to see detailed info", "&7about Gangs feature.").build(() -> {
 				this.close();
 				new GangsHelpGui(this.getPlayer()).open();
 			}));
 			//Gems
-			this.setItem(21, ItemStackBuilder.of(Material.EMERALD).name("&e&lGems").lore("&7Click to see detailed info", "&7about Gems feature.").build(() -> {
+			this.setItem(15, ItemStackBuilder.of(Material.EMERALD).name("&e&lGems").lore("&7Click to see detailed info", "&7about Gems feature.").build(() -> {
 				this.close();
 				new GemsHelpGui(this.getPlayer()).open();
 			}));
 			//Ranks
-			this.setItem(22, ItemStackBuilder.of(SkullUtils.DIAMOND_R_SKULL.clone()).name("&e&lRanks").lore("&7Click to see detailed info", "&7about Ranks feature.").build(() -> {
+			this.setItem(19, ItemStackBuilder.of(SkullUtils.DIAMOND_R_SKULL.clone()).name("&e&lRanks").lore("&7Click to see detailed info", "&7about Ranks feature.").build(() -> {
 				this.close();
 				new RanksHelpGui(this.getPlayer()).open();
 			}));
 			//Prestiges
-			this.setItem(23, ItemStackBuilder.of(SkullUtils.DIAMOND_P_SKULL.clone()).name("&e&lPrestiges").lore("&7Click to see detailed info", "&7about Prestiges feature.").build(() -> {
+			this.setItem(20, ItemStackBuilder.of(SkullUtils.DIAMOND_P_SKULL.clone()).name("&e&lPrestiges").lore("&7Click to see detailed info", "&7about Prestiges feature.").build(() -> {
 				this.close();
 				new PrestigesHelpGui(this.getPlayer()).open();
 			}));
 			//PickaxeLevels
-			this.setItem(24, ItemStackBuilder.of(CompMaterial.EXPERIENCE_BOTTLE.toItem()).name("&e&lPickaxe Levels").lore("&7Click to see detailed info", "&7about Pickaxe leveling feature.").build(() -> {
+			this.setItem(21, ItemStackBuilder.of(CompMaterial.EXPERIENCE_BOTTLE.toItem()).name("&e&lPickaxe Levels").lore("&7Click to see detailed info", "&7about Pickaxe leveling feature.").build(() -> {
 				this.close();
 				new PickaxeLevelsHelpGui(this.getPlayer()).open();
 			}));
 			//Tokens
-			this.setItem(30, ItemStackBuilder.of(SkullUtils.COIN_SKULL.clone()).name("&e&lTokens").lore("&7Click to see detailed info", "&7about Tokens feature.").build(() -> {
+			this.setItem(22, ItemStackBuilder.of(SkullUtils.COIN_SKULL.clone()).name("&e&lTokens").lore("&7Click to see detailed info", "&7about Tokens feature.").build(() -> {
 				this.close();
 				new TokensHelpGui(this.getPlayer()).open();
 			}));
 			//Multipliers
-			this.setItem(31, ItemStackBuilder.of(Material.GOLD_INGOT).name("&e&lMultipliers").lore("&7Click to see detailed info", "&7about Multipliers feature.").build(() -> {
+			this.setItem(23, ItemStackBuilder.of(Material.GOLD_INGOT).name("&e&lMultipliers").lore("&7Click to see detailed info", "&7about Multipliers feature.").build(() -> {
 				this.close();
 				new MultipliersHelpGui(this.getPlayer()).open();
 			}));
-			//Multipliers
-			this.setItem(32, ItemStackBuilder.of(Material.DIAMOND_ORE).name("&e&lMines").lore("&7Click to see detailed info", "&7about Mines feature.").build(() -> {
+			//Mines
+			this.setItem(24, ItemStackBuilder.of(Material.DIAMOND_ORE).name("&e&lMines").lore("&7Click to see detailed info", "&7about Mines feature.").build(() -> {
 				this.close();
 				new MinesHelpGui(this.getPlayer()).open();
+			}));
+			//History
+			this.setItem(25, ItemStackBuilder.of(CompMaterial.BOOK.toItem()).name("&e&lHistory").lore("&7Click to see detailed info", "&7about History feature.").build(() -> {
+				this.close();
+				new HistoryHelpGui(this.getPlayer()).open();
 			}));
 
 			this.setItem(36, ItemStackBuilder.of(Material.BARRIER).name("&c&lClose").lore("&7Click to close the gui.").build(this::close));
