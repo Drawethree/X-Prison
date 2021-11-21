690065bcef1d63c576bfc525faf1777ef9c54f2b
Drawethree <janci.kluka@gmail.com>
2021-11-21 02:32:45 +0100

    1.9.0-BETA Added comments for API methods -  UltraPrisonMinesAPI

--- core/src/main/java/me/drawethree/ultraprisoncore/mines/api/UltraPrisonMinesAPI.java
+++ core/src/main/java/me/drawethree/ultraprisoncore/mines/api/UltraPrisonMinesAPI.java
@@ -5,7 +5,19 @@ import org.bukkit.Location;
 
 public interface UltraPrisonMinesAPI {
 
+	/**
+	 * Gets a mine by name
+	 *
+	 * @param name String
+	 * @return Mine.class
+	 */
 	Mine getMineByName(String name);
 
+	/**
+	 * Gets a mine by location
+	 *
+	 * @param loc Location
+	 * @return Mine.class
+	 */
 	Mine getMineAtLocation(Location loc);
 }
