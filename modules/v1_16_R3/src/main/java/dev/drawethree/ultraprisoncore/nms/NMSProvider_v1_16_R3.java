00416b79f652d2dc91e1b52d6262f2bfe36b2b91
Drawethree <janci.kluka@gmail.com>
2022-08-22 00:56:06 +0200

    1.12.3 Small code fixes Moved libraries Updated poms

--- modules/v1_16_R3/src/main/java/dev/drawethree/ultraprisoncore/nms/NMSProvider_v1_16_R3.java
+++ modules/v1_16_R3/src/main/java/dev/drawethree/ultraprisoncore/nms/NMSProvider_v1_16_R3.java
@@ -8,7 +8,7 @@ import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
 import org.bukkit.entity.Player;
 
-public class NMSProvider_v1_16_R3 extends NMSProvider {
+public final class NMSProvider_v1_16_R3 extends NMSProvider {
 
 	@Override
 	public void setBlockInNativeDataPalette(World world, int x, int y, int z, int blockId, byte data, boolean applyPhysics) {
