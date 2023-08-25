e220c131c7193b789e7a09de46e26fa21b54705e
VoChiDanh <vochidanh.hs.c@gmail.com>
2023-08-25 15:37:14 +0700

    undo reformat code

--- src/test/java/dev/drawethree/xprison/utils/misc/MathUtilsTest.java
+++ src/test/java/dev/drawethree/xprison/utils/misc/MathUtilsTest.java
@@ -12,27 +12,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathUtilsTest {
 
-    private Map<Double, String> numbersToTest;
+	private Map<Double, String> numbersToTest;
 
-    @BeforeEach
-    public void before() {
-        this.numbersToTest = new HashMap<>();
+	@BeforeEach
+	public void before() {
+		this.numbersToTest = new HashMap<>();
 
-        this.numbersToTest.put(500.0, "500.0");
-        this.numbersToTest.put(1000.0, "1000.0");
-        this.numbersToTest.put(5000.0, "5.0k");
-        this.numbersToTest.put(5500.0, "5.5k");
+		this.numbersToTest.put(500.0, "500.0");
+		this.numbersToTest.put(1000.0, "1000.0");
+		this.numbersToTest.put(5000.0, "5.0k");
+		this.numbersToTest.put(5500.0, "5.5k");
 
-        this.numbersToTest.put(-500.0, "-500.0");
-        this.numbersToTest.put(-1000.0, "-1000.0");
-        this.numbersToTest.put(-5000.0, "-5.0k");
-        this.numbersToTest.put(-5500.0, "-5.5k");
-    }
+		this.numbersToTest.put(-500.0, "-500.0");
+		this.numbersToTest.put(-1000.0, "-1000.0");
+		this.numbersToTest.put(-5000.0, "-5.0k");
+		this.numbersToTest.put(-5500.0, "-5.5k");
+	}
 
-    @Test
-    public void formatNumbers() {
-        for (Map.Entry<Double, String> entry : this.numbersToTest.entrySet()) {
-            assertEquals(entry.getValue(), formatNumber(entry.getKey()));
-        }
-    }
+	@Test
+	public void formatNumbers() {
+		for (Map.Entry<Double, String> entry : this.numbersToTest.entrySet()) {
+			assertEquals(entry.getValue(), formatNumber(entry.getKey()));
+		}
+	}
 }
